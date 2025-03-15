b88a60c550a16ae23f229a666978b1bd1f8ca90f
Juan Jose Bergaño Arboleda <juanjoberganoarboleda@gmail.com>
2025-03-14 21:28:07 -0500

    Se agrega el atributo paisOrigen en la clase autor

--- src/autonoma/biblioteca/models/Autor.java
+++ src/autonoma/biblioteca/models/Autor.java
@@ -23,13 +23,39 @@ public class Autor extends Persona {
      */
     private String profesion;
     
+    private string paisOrigen;
+    
     ///////////////////////////////////////////////////////////////
     /// Método constructor
 
-    public Autor(String editorial, String profesion, String nombre, String documentoIdentidad, String correoElectronico) {
+    public Autor(String editorial, String profesion, String nombre, String documentoIdentidad, String correoElectronico, String paisOrigen) {
         super(nombre, documentoIdentidad, correoElectronico);
         this.editorial = editorial;
         this.profesion = profesion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public string getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public void setPaisOrigen(string paisOrigen) {
+        this.paisOrigen = paisOrigen;
     }
 
    
